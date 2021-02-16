b91139ac766c0ab983f57e6dbf67788191476609
AlekzzR <alekzzr@gmail.com>
2021-02-16 20:45:42 +0300

    Переработана созданная вчера версия

--- doc to pdf.py
+++ doc to pdf.py
@@ -1,24 +1,26 @@
 import os
 import comtypes.client
-#5
+
 wdFormatPDF = 17  # number of format in M.Word application
 
-input_dir = r'C:\Users\alekz\OneDrive\Рабочий стол\Загрузка ФГИС\Февраль\Бывалово\Январь\docx'
-output_dir = r'C:\Users\alekz\OneDrive\Рабочий стол\Загрузка ФГИС\Февраль\Бывалово\Январь\pdf'
-try:
-    os.stat(output_dir)  # check specified directory
-except:
-    os.mkdir(output_dir)  # create a directory if dosnt have
-for root, dirs, files in os.walk(input_dir):
+input_dir = r'C:\Users\alekz\OneDrive\Рабочий стол\doc_to_pdf\Архив ПТМ'
+output_dir = r'C:\Users\alekz\OneDrive\Рабочий стол\doc_to_pdf\Архив ИЦ'
+
+for dirs, dirs_name, files in os.walk(input_dir):
+    try:
+        os.stat(dirs.replace('Архив ПТМ', 'Архив ИЦ'))  # check specified directory
+    except:
+        os.mkdir(dirs.replace('Архив ПТМ', 'Архив ИЦ'))  # create a directory if dosnt have
     for file in files:
-        in_file = os.path.join(root, file)
-        print(file)
+        in_file = os.path.join(dirs, file)
+        print('Найден файл:', file)
+        print('Идет процесс конвертации в PDF')
         output_file = file.split('.')[0]  # splits the str by 2, makes a list, and passes the first str to the variable
-        out_file = output_dir + '\\' + output_file + '.pdf'
-        print(out_file)
+        out_file = dirs + '\\' + output_file + '.pdf'
         word = comtypes.client.CreateObject('Word.Application', dynamic=True)  # without "dynamic" dont work
         word.Visible = False  # works together with both True and False
         doc = word.Documents.Open(in_file)
-        doc.SaveAs(out_file, wdFormatPDF)
+        doc.SaveAs(out_file.replace('Архив ПТМ', 'Архив ИЦ'), wdFormatPDF)
         doc.Close()
         word.Quit()
+        print('Создан файл:', out_file.replace('Архив ПТМ', 'Архив ИЦ'))
