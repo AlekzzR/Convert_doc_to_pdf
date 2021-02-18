9f4ad793043a06ecd50bbc746a197ba62ffe1f5a
AlekzzR <alekzzr@gmail.com>
2021-02-18 22:02:28 +0300

    Поправил GUI

--- doc_to_pdf.py
+++ doc_to_pdf.py
@@ -30,35 +30,31 @@ class MyGUI:
             return self.directory
 
     def loop(self):
-        loop_pdf(self.directory)
+       for dirs, dirs_name, files in os.walk(self.directory.replace('/', '\\')):
+        try:
+            os.stat(dirs.replace('Архив ПТМ', 'Архив ИЦ'))  # check specified directory
+        except:
+            os.mkdir(dirs.replace('Архив ПТМ', 'Архив ИЦ'))  # create a directory if doesn't have
+        for file in files:
+            check_file = file.split('.')[1]
+            if check_file == 'doc' or check_file == 'docx':
+                print('Найден файл:', file)
+                print('Идет процесс конвертации в PDF')
+                output_file = file.split('.')[0]
+                # splits the str by 2, makes a list, and passes the first str to the var
+                out_file = dirs + '\\' + output_file + '.pdf'
+                word = comtypes.client.CreateObject('Word.Application', dynamic=True)
+                # without "dynamic" don't work
+                word.Visible = False  # works together with both True and False
+                in_file = os.path.join(dirs, file)
+                doc = word.Documents.Open(in_file)
+                doc.SaveAs(out_file.replace('Архив ПТМ', 'Архив ИЦ'), wdFormatPDF)
+                doc.Close()
+                word.Quit()
+                print('Создан файл:', out_file.replace('Архив ПТМ', 'Архив ИЦ'))
+                # counter += 1
+                print('______________')
 
-def loop_pdf(dir):
-   for dirs, dirs_name, files in os.walk(dir.replace('/', '\\')):
-    try:
-        os.stat(dirs.replace('Архив ПТМ', 'Архив ИЦ'))  # check specified directory
-    except:
-        os.mkdir(dirs.replace('Архив ПТМ', 'Архив ИЦ'))  # create a directory if doesn't have
-    for file in files:
-        check_file = file.split('.')[1]
-        if check_file == 'doc' or check_file == 'docx':
-            print('Найден файл:', file)
-            print('Идет процесс конвертации в PDF')
-            output_file = file.split('.')[0]
-            # splits the str by 2, makes a list, and passes the first str to the var
-            out_file = dirs + '\\' + output_file + '.pdf'
-            word = comtypes.client.CreateObject('Word.Application', dynamic=True)
-            # without "dynamic" don't work
-            word.Visible = False  # works together with both True and False
-            in_file = os.path.join(dirs, file)
-            doc = word.Documents.Open(in_file)
-            doc.SaveAs(out_file.replace('Архив ПТМ', 'Архив ИЦ'), wdFormatPDF)
-            doc.Close()
-            word.Quit()
-            print('Создан файл:', out_file.replace('Архив ПТМ', 'Архив ИЦ'))
-            # counter += 1
-            print('______________')
-#input_dir = r'C:\Users\alekz\OneDrive\Рабочий стол\doc_to_pdf\Архив ПТМ'
-#output_dir = r'C:\Users\alekz\OneDrive\Рабочий стол\doc_to_pdf\Архив ИЦ'
 
 
 
