e1357d2aef3bf9e8c103646c80aea105ebc05c38
AlekzzR <alekzzr@gmail.com>
2021-02-18 21:53:52 +0300

    Сделал GUI

--- doc_to_pdf.py
+++ doc_to_pdf.py
@@ -1,14 +1,39 @@
 import os
-
+import tkinter as tk
 import comtypes.client
+import tkinter.filedialog as fd
 
 wdFormatPDF = 17  # number of format in M.Word application
 counter = 0
 
-input_dir = r'C:\Users\alekz\OneDrive\Рабочий стол\doc_to_pdf\Архив ПТМ'
-output_dir = r'C:\Users\alekz\OneDrive\Рабочий стол\doc_to_pdf\Архив ИЦ'
+class MyGUI:
+    def __init__(self):
+        self.main_window = tk.Tk()
+        self.label1 = tk.Label(self.main_window, text='  ')
+        self.label2 = tk.Label(self.main_window, text='Программа для конвертации файлов doc в pdf')
+        self.label3 = tk.Label(self.main_window, text='  ')
+        self.label4 = tk.Label(self.main_window, text='  ')
+        self.button_dir = tk.Button(self.main_window, text='Выбрать папку', command=self.choose_directory)
+        self.button_start = tk.Button(self.main_window, text='Погнали', command=self.loop)
+        self.label2.pack()
+        self.label1.pack()
+        self.button_dir.pack()
+        self.label3.pack()
+        self.button_start.pack()
+        self.label4.pack()
+        tk.mainloop()
 
-for dirs, dirs_name, files in os.walk(input_dir):
+    def choose_directory(self):
+        self.directory = fd.askdirectory(title="Открыть папку", initialdir="/")
+        if self.directory:
+            print(self.directory)
+            return self.directory
+
+    def loop(self):
+        loop_pdf(self.directory)
+
+def loop_pdf(dir):
+   for dirs, dirs_name, files in os.walk(dir.replace('/', '\\')):
     try:
         os.stat(dirs.replace('Архив ПТМ', 'Архив ИЦ'))  # check specified directory
     except:
@@ -18,9 +43,11 @@ for dirs, dirs_name, files in os.walk(input_dir):
         if check_file == 'doc' or check_file == 'docx':
             print('Найден файл:', file)
             print('Идет процесс конвертации в PDF')
-            output_file = file.split('.')[0]  # splits the str by 2, makes a list, and passes the first str to the var
+            output_file = file.split('.')[0]
+            # splits the str by 2, makes a list, and passes the first str to the var
             out_file = dirs + '\\' + output_file + '.pdf'
-            word = comtypes.client.CreateObject('Word.Application', dynamic=True)  # without "dynamic" don't work
+            word = comtypes.client.CreateObject('Word.Application', dynamic=True)
+            # without "dynamic" don't work
             word.Visible = False  # works together with both True and False
             in_file = os.path.join(dirs, file)
             doc = word.Documents.Open(in_file)
@@ -28,12 +55,18 @@ for dirs, dirs_name, files in os.walk(input_dir):
             doc.Close()
             word.Quit()
             print('Создан файл:', out_file.replace('Архив ПТМ', 'Архив ИЦ'))
-            counter += 1
+            # counter += 1
             print('______________')
-print('Success! Program converted', counter, 'files')
-print(input('Press ENTER to exit'))
+#input_dir = r'C:\Users\alekz\OneDrive\Рабочий стол\doc_to_pdf\Архив ПТМ'
+#output_dir = r'C:\Users\alekz\OneDrive\Рабочий стол\doc_to_pdf\Архив ИЦ'
+
+
+
+my_gui = MyGUI()
+#print('Success! Program converted', counter, 'files')
+#print(input('Press ENTER to exit'))
 
 # todo:
-# 0. Сделать GUI
+# 0. Вывести информационное диалоговое окно по окончании со статистикой
 # 1. Сделать прогресс бар
 # 2. Прикрутить кнопку Отмена
