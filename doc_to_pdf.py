0278e9ce2c0485e28d1c2f5f53a326149324f019
AlekzzR <alekzzr@gmail.com>
2021-02-19 23:16:47 +0300

    Добавлена кнопка Выход Добавлены информационные окна

--- doc_to_pdf.py
+++ doc_to_pdf.py
@@ -1,5 +1,6 @@
 import os
 import tkinter as tk
+import tkinter.messagebox
 import comtypes.client
 import tkinter.filedialog as fd
 
@@ -8,19 +9,24 @@ counter = 0
 
 class MyGUI:
     def __init__(self):
+        self.counter = 0
         self.main_window = tk.Tk()
         self.label1 = tk.Label(self.main_window, text='  ')
         self.label2 = tk.Label(self.main_window, text='Программа для конвертации файлов doc в pdf')
         self.label3 = tk.Label(self.main_window, text='  ')
         self.label4 = tk.Label(self.main_window, text='  ')
-        self.button_dir = tk.Button(self.main_window, text='Выбрать папку', command=self.choose_directory)
-        self.button_start = tk.Button(self.main_window, text='Погнали', command=self.loop)
+        self.label5 = tk.Label(self.main_window, text='  ')
+        self.button_dir = tk.Button(self.main_window, text='  Выбрать папку  ', command=self.choose_directory)
+        self.button_start = tk.Button(self.main_window, text='  Погнали  ', command=self.loop)
+        self.button_exit = tk.Button(self.main_window,text='  Выход  ', command=self.main_window.destroy)
         self.label2.pack()
         self.label1.pack()
         self.button_dir.pack()
         self.label3.pack()
         self.button_start.pack()
         self.label4.pack()
+        self.button_exit.pack()
+        self.label5.pack()
         tk.mainloop()
 
     def choose_directory(self):
@@ -30,31 +36,39 @@ class MyGUI:
             return self.directory
 
     def loop(self):
-       for dirs, dirs_name, files in os.walk(self.directory.replace('/', '\\')):
-        try:
-            os.stat(dirs.replace('Архив ПТМ', 'Архив ИЦ'))  # check specified directory
-        except:
-            os.mkdir(dirs.replace('Архив ПТМ', 'Архив ИЦ'))  # create a directory if doesn't have
-        for file in files:
-            check_file = file.split('.')[1]
-            if check_file == 'doc' or check_file == 'docx':
-                print('Найден файл:', file)
-                print('Идет процесс конвертации в PDF')
-                output_file = file.split('.')[0]
-                # splits the str by 2, makes a list, and passes the first str to the var
-                out_file = dirs + '\\' + output_file + '.pdf'
-                word = comtypes.client.CreateObject('Word.Application', dynamic=True)
-                # without "dynamic" don't work
-                word.Visible = False  # works together with both True and False
-                in_file = os.path.join(dirs, file)
-                doc = word.Documents.Open(in_file)
-                doc.SaveAs(out_file.replace('Архив ПТМ', 'Архив ИЦ'), wdFormatPDF)
-                doc.Close()
-                word.Quit()
-                print('Создан файл:', out_file.replace('Архив ПТМ', 'Архив ИЦ'))
-                # counter += 1
-                print('______________')
-
+       try:
+           for dirs, dirs_name, files in os.walk(self.directory.replace('/', '\\')):
+               try:
+                   os.stat(dirs.replace('Архив ПТМ', 'Архив ИЦ'))  # check specified directory
+               except:
+                   os.mkdir(dirs.replace('Архив ПТМ', 'Архив ИЦ'))  # create a directory if doesn't have
+               for file in files:
+                   check_file = file.split('.')[1]
+                   if check_file == 'doc' or check_file == 'docx':
+                       print('Найден файл:', file)
+                       print('Идет процесс конвертации в PDF')
+                       output_file = file.split('.')[0]
+                       # splits the str by 2, makes a list, and passes the first str to the var
+                       out_file = dirs + '\\' + output_file + '.pdf'
+                       word = comtypes.client.CreateObject('Word.Application', dynamic=True)
+                       # without "dynamic" don't work
+                       word.Visible = False  # works together with both True and False
+                       in_file = os.path.join(dirs, file)
+                       doc = word.Documents.Open(in_file)
+                       doc.SaveAs(out_file.replace('Архив ПТМ', 'Архив ИЦ'), wdFormatPDF)
+                       doc.Close()
+                       word.Quit()
+                       print('Создан файл:', out_file.replace('Архив ПТМ', 'Архив ИЦ'))
+                       self.counter += 1
+                       print('______________')
+           self.show_info()
+           print('Преобразовано', self.counter, "файлов")
+       except AttributeError:
+           self.show_error()
+    def show_info(self):
+       tk.messagebox.showinfo('Информация', 'Преобразовано файлов: ' + str(self.counter))
+    def show_error(self):
+       tk.messagebox.showinfo('Ошибка', 'Не выбрана папка с файлами')
 
 
 
@@ -64,5 +78,6 @@ my_gui = MyGUI()
 
 # todo:
 # 0. Вывести информационное диалоговое окно по окончании со статистикой
+# 0.1 Прикрутить кнопку Выход
 # 1. Сделать прогресс бар
 # 2. Прикрутить кнопку Отмена
